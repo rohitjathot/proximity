1db51fe7d6168415536b369e64c4c4d702b2923c
rohit jathot <rohitjaghot7@gmail.com>
2021-11-20 17:18:29 +0530

    first commit

---
+++ src/main/java/com/phone/proximity/ProximityApplication.java
@@ -0,0 +1,13 @@
+package com.phone.proximity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProximityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProximityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phone/proximity/ProximityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phone.proximity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProximityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
